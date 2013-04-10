92393b89dca901019ad3eca08bdea7a7c30f487d
Naoto Horiguchi <naoto.horiguchi@gmail.com>
2013-04-10 16:00:26 +0900

    Initial Commit

---
+++ AHCImagesPicker/AHCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AHCAppDelegate.h
+//  AHCImagesPicker
+//
+//  Created by Naoto Horiguchi on 2013/04/10.
+//  Copyright (c) 2013年 Naoto Horiguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AHCImagesPicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AHCImagesPicker
+//
+//  Created by Naoto Horiguchi on 2013/04/10.
+//  Copyright (c) 2013年 Naoto Horiguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AHCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AHCAppDelegate class]));
+    }
+}
